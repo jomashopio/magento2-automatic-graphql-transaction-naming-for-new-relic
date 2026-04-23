0b788433ecb6135e27dca845827b0532877f4657
Steven Hoffman <steven@jomashop.io>
2026-04-23 12:56:15 -0400

    2.4.7 Support - Parse the data from ast if DocumentNode is passed Helper/NewRelicReportData.php modified: Parse the data from ast if DocumentNode is passed Plugin/NewRelicMonitoring.php modified: Add DocumentNode as possible type (Changes the fieldCount and fieldNames if passed a DocumentNode - fieldCount now counts all the fields - fieldNames now returns the operations' fields - Prior data appears to have been some special fields in the schema - The operation name is expected to the same in the case of single operation with name)

--- Plugin/NewRelicMonitoring.php
+++ Plugin/NewRelicMonitoring.php
@@ -5,6 +5,7 @@
 
 namespace JomaShop\NewRelicMonitoring\Plugin;
 
+use GraphQL\Language\AST\DocumentNode;
 use Magento\Framework\GraphQl\Query\QueryProcessor;
 use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 use Magento\Framework\GraphQl\Schema;
@@ -46,7 +47,7 @@ class NewRelicMonitoring
     public function beforeProcess(
         QueryProcessor $subject,
         Schema $schema,
-        string $source,
+        DocumentNode|string $source,
         ContextInterface $contextValue = null,
         array $variableValues = null,
         string $operationName = null

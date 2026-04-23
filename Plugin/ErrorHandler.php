7fa527cadd8a55991958ca5eb022aced6353e65d
Steven Hoffman <steven@jomashop.io>
2026-04-23 12:55:22 -0400

    Plugin/ErrorHandler.php - Use the inner error if error is aggregate (Also log the outer error as a custom param ExceptionAggregateMessage)

--- Plugin/ErrorHandler.php
+++ Plugin/ErrorHandler.php
@@ -4,36 +4,62 @@
  */
 namespace JomaShop\NewRelicMonitoring\Plugin;
 
+use Magento\Framework\Exception\AggregateExceptionInterface;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\GraphQl\Query\ErrorHandler as QueryErrorHandler;
 use Magento\NewRelicReporting\Model\NewRelicWrapper;
+use Throwable;
 
 class ErrorHandler
 {
-    /**
-     * @var NewRelicWrapper
-     */
-    private $newRelicWrapper;
-
-    /**
-     * @param NewRelicWrapper $newRelicWrapper
-     */
     public function __construct(
-        NewRelicWrapper $newRelicWrapper
+        private readonly NewRelicWrapper $newRelicWrapper,
     ) {
-        $this->newRelicWrapper = $newRelicWrapper;
     }
 
     public function beforeHandle(QueryErrorHandler $errorHandler, array $errors, callable $formatter)
     {
-        if (empty($errors)) {
+        // We want to find the first error that has a previous
+        // - errors are expected to be wrapped by graphql
+        $firstPreviousError = array_filter(
+            array_map(
+                fn($error) => $error->getPrevious(),
+                $errors
+            ),
+        )[0] ?? null;
+        if (!$firstPreviousError) {
             return;
         }
 
-        foreach ($errors as $error) {
-            $previousError = $error->getPrevious();
-            if ($previousError instanceof \Exception) {
-                $this->newRelicWrapper->reportError($previousError);
-            }
+        // Get error data
+        $errorData = $this->getErrorData($firstPreviousError);
+
+        // Report data
+        $this->newRelicWrapper->reportError($errorData['errorToReport']);
+        foreach ($errorData['extraData'] as $key => $value) {
+            $this->newRelicWrapper->addCustomParameter($key, $value);
         }
+    }
+
+    private function getErrorData(Throwable $error): array
+    {
+        // Get inner exception if available
+        // - See eg: \Magento\QuoteGraphQl\Model\Cart\AddSimpleProductToCart::execute
+        $firstInnerError = $error instanceof AggregateExceptionInterface
+            ? $error->getErrors()[0] ?? null
+            : null;
+
+        // Prepare data
+        $data = [
+            'errorToReport' => $firstInnerError ?? $error,
+            'extraData' => []
+        ];
+
+        // Add Aggregate exception message (As we are using the first inner error to report)
+        if ($firstInnerError instanceof AggregateExceptionInterface) {
+            $data['extraData']['ExceptionAggregateMessage'] = $firstInnerError->getMessage();
+        }
+
+        return $data;
     }
 }
